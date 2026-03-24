a12788c9575fbf1df64ee46b877ddee969d448bd
Arga Marihot <argamarihots@gmail.com>
2026-03-24 13:29:44 +0700

    fix app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # =========================
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-
+APP_PASSWORD = st.secrets["APP_PASSWORD"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # =========================
